033dc5811c174c012f957458d3eff70b91e0449f
ERostovskaya <ERostovskaya@IBS.RU>
2023-11-30 19:29:27 +0300

    Task 2 - Data Types

---
+++ src/com/max/types/Main.java
@@ -0,0 +1,11 @@
+package com.max.types;
+
+public class Main {
+    public static void main(String[] args) {
+        double d = 1.3333333333333334;
+        System.out.println(d);
+
+        byte b = 127;
+        System.out.println(b);
+    }
+}
